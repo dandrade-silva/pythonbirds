dc21a8fa34970c73ddab388fca1822d4f6b6056a
Danilo Andrade <dandrade0803@gmail.com>
2023-07-13 02:51:11 -0300

    OO Aula Atributo de Instância

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -3,11 +3,24 @@
 # Exemplos: carros = Carros ou classe carro = ClasseCarro
 
 class Pessoa:
+    # Atributo especial. Ele é executado quando você construi o objeto
+    # Ele é conhecido também como método construtor
+    def __init__(self, nome=None, idade=30):
+        self.nome = nome
+        self.idade = idade
+
     # Método é uma função que pertence há uma classe, portanto, está sempre ligada a classe.
-    def cumprimentar(self):
-        return 'Olá'
+
+    def cumprimentar(self):  # Atributo da classe
+        return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Miguel')
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Danilo'
+    print(p.nome)
+    print(p.idade)
