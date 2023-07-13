a8591481454f4896e6ae58037e5cb98ac529591d
Danilo Andrade <dandrade0803@gmail.com>
2023-07-13 02:11:25 -0300

    OO Aula Classes

---
+++ oo/pessoa.py
@@ -0,0 +1,6 @@
+# Classe é a base para Orientação a Objetos
+# De acordo com a PEP8, as classes devem começar com letras maiusculas
+# Exemplos: carros = Carros ou classe carro = ClasseCarro
+
+class Pessoa:
+    pass
