7c14a136a68148589206d7272c54675cea2f8763
Danilo Andrade <dandrade0803@gmail.com>
2023-07-13 09:17:37 -0300

    OO Aula Atributo Complexo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -3,24 +3,25 @@
 # Exemplos: carros = Carros ou classe carro = ClasseCarro
 
 class Pessoa:
-    # Atributo especial. Ele é executado quando você construi o objeto
-    # Ele é conhecido também como método construtor
-    def __init__(self, nome=None, idade=30):
+    def __init__(self, *filhos, nome=None, idade=30):
+        # Atributo especial. Ele é executado quando você construi o objeto.
+        # Ele também é conhecido como método construtor
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
-    # Método é uma função que pertence há uma classe, portanto, está sempre ligada a classe.
-
-    def cumprimentar(self):  # Atributo da classe
+    def cumprimentar(self):  # Método da classe
+        # Método é uma função que pertence há uma classe.
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Miguel')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Danilo'
-    print(p.nome)
-    print(p.idade)
+    miguel = Pessoa(nome='Miguel', idade=4)
+    danilo = Pessoa(miguel, nome='Danilo', idade=30)
+    print(Pessoa.cumprimentar(danilo))
+    print(id(danilo))
+    print(danilo.cumprimentar())
+    print(danilo.nome)
+    print(danilo.idade)
+    for filho in danilo.filhos:
+        print(filho.nome)
